ffb1838fb48d707cbc8e0e0a9e1f8b08feaf3259
bprinty <bprinty@gmail.com>
2020-02-27 17:19:55 -0600

    Jotting down quick notes on implementation for models

--- src/model.js
+++ src/model.js
@@ -4,24 +4,46 @@
  */
 export default class Model {
 
+  constructor(data) {
+    this.contract = this.props();
+    this.endpoints = this.api();
+
+    // model props
+    this.id = null;
+
+    // store snapshot
+    this.$ = Proxy({}, {
+      get: (obj, prop) => {
+        // // look into using store.watch() for watching state changes for model instance
+        // store.getters(`${name}`, this.id).prop;
+      },
+      set: (obj, prop, value) => {
+        throw 'Cannot set properties directly on the store';
+      }
+    });
+  }
+
   /**
    * API config for fetching and updating data.
    */
-  static api() {
-    return {};
+  api() {
+    return {
+      model: null,
+      collection: null,
+    };
   }
 
   /**
    * Property definitions for the model.
    */
-  static props() {
+  props() {
     return {};
   }
 
   /**
    * Relationships to other objects tracked by the orm.
    */
-  static relationships() {
+  relationships() {
     return {};
   }
 
