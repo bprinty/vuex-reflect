329f4a4b2e32572f18c42ee87a99ee4ae7da542e
bprinty <bprinty@gmail.com>
2020-02-29 05:42:09 -0600

    Removed unecessary docs folder

--- docs/api/config.js
+++
@@ -1,1 +0,0 @@
-exports.fileTree=[{"name":"_index","path":"/_index","fullPath":"docs/api/_index"}];exports.sidebarTree = (title = 'Mainpage') => ({"/api/":[{"title":"API","collapsable":false,"children":[["",""+title+""],"_index"]}]});
