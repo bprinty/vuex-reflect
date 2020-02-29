eefed9c2fe6df105637878b7c2a18ffd3658315e
bprinty <bprinty@gmail.com>
2020-02-29 04:47:36 -0600

    Auto-linting

--- src/plugin.js
+++ src/plugin.js
@@ -8,7 +8,6 @@ import axios from 'axios';
  * Main entrypoint for Vuex Reflect plugin.
  */
 export default function Reflect(models) {
-
   // sanitize inputs
 
   // configure options
@@ -31,7 +30,7 @@ export default function Reflect(models) {
         mutations: {
           [`${key}.add`]: (state, data) => {
             state[key] = _.clone(state[key]);
-            state[key][data.id] = data
+            state[key][data.id] = data;
           },
           [`${key}.update`]: (state, data) => {
             state[key] = _.clone(state[key]);
@@ -43,44 +42,34 @@ export default function Reflect(models) {
           },
         },
         actions: {
-          [`${key}.fetch`]: ({ state, commit }) => {
-            return axios.get(`/${key}`).then((response) => {
-              const results = [];
-              response.data.forEach((item) => {
-                commit(`${key}.add`, item);
-                results.push(state[key][item.id]);
-              });
-              return results;
+          [`${key}.fetch`]: ({ state, commit }) => axios.get(`/${key}`).then((response) => {
+            const results = [];
+            response.data.forEach((item) => {
+              commit(`${key}.add`, item);
+              results.push(state[key][item.id]);
             });
-          },
-          [`${key}.create`]: ({ state, commit }, data) => {
-            return axios.post(`/${key}`, data).then((response) => {
-              commit(`${key}.add`, response.data);
-              return state[key][response.data.id];
-            });
-          },
-          [`${key}.update`]: ({ state, commit }, data) => {
-            return axios.put(`/${key}/${data.id}`, data).then((response) => {
-              commit(`${key}.update`, response.data);
-              return state[key][response.data.id];
-            });
-          },
-          [`${key}.get`]: ({ state, commit }, id) => {
-            return axios.get(`/${key}/${id}`).then((response) => {
-              commit(`${key}.update`, response.data);
-              return state[key][response.data.id];
-            });
-          },
-          [`${key}.delete`]: ({ state, commit }, id) => {
-            return axios.delete(`/${key}/${id}`).then((response) => {
-              commit(`${key}.remove`, id);
-              return null;
-            });
-          }
+            return results;
+          }),
+          [`${key}.create`]: ({ state, commit }, data) => axios.post(`/${key}`, data).then((response) => {
+            commit(`${key}.add`, response.data);
+            return state[key][response.data.id];
+          }),
+          [`${key}.update`]: ({ state, commit }, data) => axios.put(`/${key}/${data.id}`, data).then((response) => {
+            commit(`${key}.update`, response.data);
+            return state[key][response.data.id];
+          }),
+          [`${key}.get`]: ({ state, commit }, id) => axios.get(`/${key}/${id}`).then((response) => {
+            commit(`${key}.update`, response.data);
+            return state[key][response.data.id];
+          }),
+          [`${key}.delete`]: ({ state, commit }, id) => axios.delete(`/${key}/${id}`).then((response) => {
+            commit(`${key}.remove`, id);
+            return null;
+          }),
         },
       });
     });
 
     return store;
-  }
+  };
 }
