5e9f344a58f94ea848c93fd5731aa7ec123a2c60
bprinty <bprinty@gmail.com>
2020-02-29 11:52:00 -0600

    Cleaning up tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -5,35 +5,14 @@
 
 // imports
 // -------
-import axios from 'axios';
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { Model, Reflect } from '../src/index';
-import mock from './mock';
-import server from './server';
-
-if (typeof assert === 'undefined') {
-  var assert = require('chai').assert;
-}
-
-// config
-// ------
-jest.mock('axios');
-server.init();
-
-beforeEach(() => {
-  // NOOP
-});
-
-afterEach(() => {
-  // NOOP
-});
+import { assert } from 'chai';
+import { Reflect } from '../src/index';
 
 
 // plugin setup
 // ------------
-const database = Reflect({});
-
 Vue.use(Vuex);
 const store = new Vuex.Store({
   state: {
@@ -51,92 +30,11 @@ const store = new Vuex.Store({
       });
     },
   },
-  plugins: [database],
+  plugins: [Reflect({})],
 });
 
 
-// api
-// ---
-describe("api mock", () => {
-  let res;
-
-  beforeEach(() => {
-    server.reset();
-  });
-
-  test("404", async () => {
-    try {
-      await axios.get('/missing');
-      assert.fail('Request returned response instead of 404.');
-    } catch (err) {
-      assert.equal(err.status, 404);
-    }
-  });
-
-  test("GET", async () => {
-    // collection
-    res = await axios.get('/posts');
-    assert.equal(res.status, 200);
-    assert.equal(res.data.length, 2);
-
-    // model
-    res = await axios.get('/posts/1');
-    assert.equal(res.status, 200);
-    assert.equal(res.data.title, 'Foo');
-  });
-
-  test("POST", async () => {
-    // create
-    const author = { name: 'Foo Bar', email: 'foo@bar.com' };
-    res = await axios.post('/authors', author);
-    assert.equal(res.status, 201);
-    assert.equal(res.data.name, 'Foo Bar');
-
-    // verify
-    res = await axios.get('/authors/3');
-    assert.equal(res.status, 200);
-    assert.equal(res.data.name, 'Foo Bar');
-  });
-
-  test("PUT", async () => {
-    // check
-    res = await axios.get('/authors/1');
-    assert.equal(res.status, 200);
-    assert.equal(res.data.name, 'Jane Doe');
-
-    // update
-    res = await axios.put('/authors/1', { name: 'test' });
-    assert.equal(res.status, 200);
-    assert.equal(res.data.name, 'test');
-
-    // verify
-    res = await axios.get('/authors/1');
-    assert.equal(res.status, 200);
-    assert.equal(res.data.name, 'test');
-  });
-
-  test("DELETE", async () => {
-    // check
-    res = await axios.get('/authors/1');
-    assert.equal(res.status, 200);
-    assert.equal(res.data.name, 'Jane Doe');
-
-    // delete
-    res = await axios.delete('/authors/1');
-    assert.equal(res.status, 204);
-
-    // verify
-    try {
-      await axios.get('/authors/1');
-      assert.fail('Request returned response instead of 404.');
-    } catch(err) {
-      assert.equal(err.status, 404);
-    }
-  });
-});
-
-
-// setup
+// tests
 // -----
 describe("store configuration", () => {
 
