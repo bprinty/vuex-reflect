ea10ca7c30535714fb39d040596d3971132e726f
bprinty <bprinty@gmail.com>
2020-02-29 11:51:02 -0600

    Abstracted plugin definitions to use factory functions for managing different access patterns

--- src/plugin.js
+++ src/plugin.js
@@ -1,7 +1,9 @@
 
 
-import _ from 'lodash';
 import axios from 'axios';
+import getterFactory from './constructs/getters';
+import mutationFactory from './constructs/mutations';
+import actionFactory from './constructs/actions';
 
 
 /**
@@ -18,54 +20,29 @@ export default function Reflect(models) {
   return (store) => {
     Object.keys(models).forEach((key) => {
       const config = models[key];
+      const get = getterFactory(config);
+      const mutate = mutationFactory(config);
+      const act = actionFactory(config);
       store.registerModule(key, {
         namespaced: false,
         state: {
-          [`${key}`]: {},
+          [`${key}`]: config.singleton ? config.default || null : {},
         },
         getters: {
-          [`${key}`]: state => _.values(state[key]),
-          [`${key}.get`]: state => id => state[key][id],
+          [`${key}`]: state => input => get.base(state, key, input),
+          [`${key}.sample`]: state => n => get.sample(state, key, n),
         },
         mutations: {
-          [`${key}.add`]: (state, data) => {
-            state[key] = _.clone(state[key]);
-            state[key][data.id] = data;
-          },
-          [`${key}.update`]: (state, data) => {
-            state[key] = _.clone(state[key]);
-            state[key][data.id] = Object.assign(state[key][data.id] || {}, data);
-          },
-          [`${key}.remove`]: (state, id) => {
-            state[key] = _.clone(state[key]);
-            delete state[key][id];
-          },
+          [`${key}.sync`]: (state, data) => mutate.sync(state, config, key, data),
+          [`${key}.reset`]: (state, id) => mutate.reset(state, config, key, id),
+          [`${key}.remove`]: (state, id) => mutate.remove(state, config, key, id),
         },
         actions: {
-          [`${key}.fetch`]: ({ state, commit }) => axios.get(`/${key}`).then((response) => {
-            const results = [];
-            response.data.forEach((item) => {
-              commit(`${key}.add`, item);
-              results.push(state[key][item.id]);
-            });
-            return results;
-          }),
-          [`${key}.create`]: ({ state, commit }, data) => axios.post(`/${key}`, data).then((response) => {
-            commit(`${key}.add`, response.data);
-            return state[key][response.data.id];
-          }),
-          [`${key}.update`]: ({ state, commit }, data) => axios.put(`/${key}/${data.id}`, data).then((response) => {
-            commit(`${key}.update`, response.data);
-            return state[key][response.data.id];
-          }),
-          [`${key}.get`]: ({ state, commit }, id) => axios.get(`/${key}/${id}`).then((response) => {
-            commit(`${key}.update`, response.data);
-            return state[key][response.data.id];
-          }),
-          [`${key}.delete`]: ({ state, commit }, id) => axios.delete(`/${key}/${id}`).then((response) => {
-            commit(`${key}.remove`, id);
-            return null;
-          }),
+          [`${key}.fetch`]: context => act.fetch(context, config, key),
+          [`${key}.create`]: (context, data) => act.create(context, config, key, data),
+          [`${key}.update`]: (context, data) => act.update(context, config, key, data),
+          [`${key}.get`]: (context, id) => act.get(context, config, key, id),
+          [`${key}.delete`]: (context, id) => act.delete(context, config, key, id),
         },
       });
     });
