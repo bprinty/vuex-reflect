7f85ed874caa8371732bb182ae5a6bdedbfd6daa
bprinty <bprinty@gmail.com>
2020-03-02 21:37:33 -0600

    Added more getter and contract testing to protect against regressions

--- tests/store/getters.test.js
+++ tests/store/getters.test.js
@@ -4,6 +4,7 @@
 
 // imports
 // -------
+import _ from 'lodash';
 import { assert } from 'chai';
 import server from '../server';
 import store from './models';
@@ -20,48 +21,62 @@ beforeEach(() => {
 
 // tests
 // -----
-describe("getters", () => {
-  let res;
+let res;
 
-  test("model", async () => {
-    // fetch collection
-    await store.dispatch("authors.fetch");
+test("getters.model", async () => {
+  // fetch collection
+  await store.dispatch("authors.fetch");
 
-    // all
-    res = store.getters.authors();
-    assert.equal(res.length, 2);
-    assert.equal(res[0].name, 'Jane Doe');
+  // all
+  res = store.getters.authors();
+  assert.equal(res.length, 2);
+  assert.equal(res[0].name, 'Jane Doe');
 
-    // subset by id
-    res = store.getters.authors([2]);
-    assert.equal(res.length, 1);
-    assert.equal(res[0].name, 'John Doe');
-    res = store.getters.authors([1, 2]);
-    assert.equal(res.length, 2);
-    assert.equal(res[0].name, 'Jane Doe');
+  // subset by id
+  res = store.getters.authors([2]);
+  assert.equal(res.length, 1);
+  assert.equal(res[0].name, 'John Doe');
+  res = store.getters.authors([1, 2]);
+  assert.equal(res.length, 2);
+  assert.equal(res[0].name, 'Jane Doe');
 
-    // one
-    res = store.getters.authors(2);
-    assert.equal(res.name, 'John Doe');
+  // one
+  res = store.getters.authors(2);
+  assert.equal(res.name, 'John Doe');
 
-    // missing
-    res = store.getters.authors(9000);
-    assert.isUndefined(res);
+  // missing
+  res = store.getters.authors(9000);
+  assert.isUndefined(res);
 
-  });
+});
 
-  test("sample", async () => {
-    // fetch collection
-    await store.dispatch("authors.fetch");
+test("getters.sample", async () => {
+  // fetch collection
+  await store.dispatch("authors.fetch");
 
-    // one
-    res = store.getters['authors.sample']();
-    assert.isTrue('name' in res);
+  // one
+  res = store.getters['authors.sample']();
+  assert.isTrue('name' in res);
 
-    // some
-    res = store.getters['authors.sample'](2);
-    assert.equal(res.length, 2);
-    assert.isTrue('name' in res[0]);
-  });
+  // some
+  res = store.getters['authors.sample'](2);
+  assert.equal(res.length, 2);
+  assert.isTrue('name' in res[0]);
+});
 
+test("getters.template", async () => {
+  res = store.getters['profile.template']();
+  assert.isTrue(_.isEqual(res, { username: '<anonymous>' }));
+
+  res = store.getters['authors.template']();
+  assert.isTrue(_.isEqual(res, { name: null, email: null }));
+
+  res = store.getters['posts.template']();
+  assert.isTrue(_.isEqual(res, {
+    slug: undefined,
+    title: 'My Post Title',
+    body: undefined,
+    footer: 'footer',
+    author: undefined
+  }));
 });
