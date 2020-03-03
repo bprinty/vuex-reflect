52f1447a4ac66020aa4b31ccd4a1e02c081c6b59
bprinty <bprinty@gmail.com>
2020-03-02 21:35:14 -0600

    Placeholder for query ideas

--- src/querying.js
+++ src/querying.js
@@ -3,7 +3,18 @@
 /**
  * Clojure for fluid query API.
  */
-export default function query() {
+export default function query(data) {
   // use fluid api for query syntax (see d3 for details)
-  return () => {};
+  this.data = data;
+  return {
+    filter: (params) => query(filter(params, this.data)),
+    first: () => this.data[0],
+    last: () => this.data[this.data.length - 1],
+    all: () => this.data,
+  };
+}
+
+
+function filter(params, data) {
+  return data;
 }
