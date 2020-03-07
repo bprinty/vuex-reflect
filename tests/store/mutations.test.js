d5da87b6903f87ef19543cc35d5295efc7d6e8fb
bprinty <bprinty@gmail.com>
2020-03-07 10:25:33 -0600

    Added more mutation tests

--- tests/store/mutations.test.js
+++ tests/store/mutations.test.js
@@ -20,43 +20,88 @@ beforeEach(() => {
 
 // tests
 // -----
+let res;
+
 test("mutations.sync", async () => {
-  let model;
+
+  // fetch data
+  res = await store.dispatch('posts.fetch');
+  res[0].title = 'test'
+
+  // get missing
+  try {
+    obj = store.getters['posts'](3);
+    assert.fail('Should not be able to access object before commit.');
+  } catch (err) {
+    assert.isTrue(err.message.length > 0);
+  }
 
   // sync one
-  assert.isTrue(true);
+  let obj = Object.assign(
+    store.getters['posts.template'](),
+    { id: 3, title: 'a' },
+  );
+  store.commit('posts.sync', obj);
+  obj = store.getters['posts'](3);
+  assert.equal(obj.id, 3);
+  assert.equal(obj.title, 'a');
 
   // sync existing
-  assert.isTrue(true);
+  obj.title = 'b';
+  store.commit('posts.sync', obj);
+  obj = store.getters['posts'](3);
+  assert.equal(obj.id, 3);
+  assert.equal(obj.title, 'b');
 
   // sync array
-  assert.isTrue(true);
+  obj.title = 'c';
+  store.commit('posts.sync', [obj]);
+  obj = store.getters['posts'](3);
+  assert.equal(obj.id, 3);
+  assert.equal(obj.title, 'c');
 
 });
 
 test("mutations.remove", async () => {
-  let model;
+
+  // fetch data
+  await store.dispatch('posts.fetch');
 
   // remove one
-  assert.isTrue(true);
+  res = store.getters['posts'](2);
+  assert.equal(res.id, 2);
+  store.commit('posts.remove', res.id);
+  try {
+    res = store.getters['posts'](2);
+    assert.fail('Should not be able to access removed object.');
+  } catch (err) {
+    assert.isTrue(err.message.length > 0);
+  }
 
   // remove multiple
-  assert.isTrue(true);
+  res = store.getters['posts'](1);
+  assert.equal(res.id, 1);
+  store.commit('posts.remove', res.id);
+  try {
+    res = store.getters['posts']([1]);
+    assert.fail('Should not be able to access removed object.');
+  } catch (err) {
+    assert.isTrue(err.message.length > 0);
+  }
 
 });
 
 
 test("mutations.clear", async () => {
-  let res;
 
   // fetch and assert store
-  await store.dispatch('posts.fetch');
-  res = store.getters['posts']()
+  await store.dispatch('authors.fetch');
+  res = store.getters['authors']()
   assert.equal(res.length, 2);
 
   // clear and assert store
-  store.commit('posts.clear');
-  res = store.getters['posts']()
+  store.commit('authors.clear');
+  res = store.getters['authors']()
   assert.equal(res.length, 0);
 
 });
