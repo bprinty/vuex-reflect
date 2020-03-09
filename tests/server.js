5585680d6baac652b1920ff643d6ffc854da2d25
bprinty <bprinty@gmail.com>
2020-03-09 07:00:14 -0500

    Better structuring of history api

--- tests/server.js
+++ tests/server.js
@@ -1,5 +1,6 @@
 
 // import
+import _ from 'lodash';
 import { MockServer, collection, model } from './mock';
 
 // database
@@ -16,10 +17,6 @@ class App extends MockServer {
           body: 'foo bar baz',
           hits: 100,
           author_id: 1,
-          history: [
-            { delta: 'foo' },
-            { delta: 'bar' },
-          ],
           archived: false,
         },
         {
@@ -30,6 +27,10 @@ class App extends MockServer {
           history: [],
           archived: true,
         },
+      ],
+      records: [
+        { delta: 'foo', post_id: 1 },
+        { delta: 'bar', post_id: 1 },
       ],
       authors: [
         { name: 'Jane Doe', email: 'jane@doe.com' },
@@ -63,10 +64,22 @@ class App extends MockServer {
       '/posts': this.collection('posts'),
       '/posts/:id': this.model('posts'),
       '/posts/:id/history': {
-        get: id => this.db.posts[id].history,
+        get: (id) => {
+          const records = Object.keys(this.db.records).map(id => this.db.records[id]);
+          return records.filter(x => x.post_id === id);
+        },
         post: (id, data) => {
-          this.db.posts[id].history.push(data);
-          return this.db.posts[id].history;
+          // TODO: THINK AB OUT CLOJURE SYNTAX WHEN ABSTRACTING
+          //       INTO NEW PACKAGE
+          // this.db.records.get(id);
+          // this.db.records.add(data);
+          // this.db.records.update(id, data);
+          // this.db.records.remove(id);
+          data.id = Number(_.max(Object.keys(this.db.records))) + 1;
+          data.post_id = id;
+          this.db.records[data.id] = data;
+          const records = Object.keys(this.db.records).map(key => this.db.records[key]);
+          return records.filter(x => x.post_id === id);
         },
       },
       '/posts/:id/archive': {
