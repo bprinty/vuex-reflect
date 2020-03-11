6eedce59b07c55fb51d8094f01539646b15bf5f3
bprinty <bprinty@gmail.com>
2020-03-11 05:57:37 -0500

    Refactored plugin setup logic to account for store modules

--- src/plugin.js
+++ src/plugin.js
@@ -12,8 +12,13 @@ import { relationFactory, actionFactory, queryFactory } from './constructs/relat
 /**
  * Main entrypoint for Vuex Reflect plugin.
  */
-export default function Reflect(models) {
-  // sanitize inputs
+export default function Reflect(args) {
+  // pull models form input
+  let modules = {'_': {}};
+  modules._ = args.models || _.omit(args, ['modules', 'options']);
+  _.each(args.modules || {}, (models, namespace) => {
+    modules[namespace] = _.clone(models);
+  });
 
   // configure global option defaults
   const options = Object.assign({
@@ -26,9 +31,9 @@ export default function Reflect(models) {
       'delete': 'delete',
       'patch': 'patch',
     }
-  }, models.options || {});
-  delete models.options;
+  }, args.options || {});
 
+  // default config for model
   const defaults = {
     name: null,
     singleton: false,
@@ -40,140 +45,159 @@ export default function Reflect(models) {
   };
 
   return (store) => {
-    let schema = {};
 
-    // populate initial schema
-    Object.keys(models).forEach((key) => {
+    // process each module
+    _.each(modules, (models, namespace) => {
 
-      // model-based definition
-      if (!_.isPlainObject(models[key])) {
-        models[key].__store__ = store;
-        models[key].__name__ = key;
-        schema[key] = {
-          singleton: models[key].prototype instanceof Singleton,
-          api: models[key].api(),
-          contract: models[key].props(),
-          relations: models[key].relations(),
-          actions: models[key].actions(),
-          queries: models[key].queries(),
-          options: models[key].options(),
-        }
-      }
+      // prefix for namespacing
+      const prefix = namespace === '_' ? '' : namespace + '/';
 
-      // store-based definition
-      else {
-        schema[key] = Object.assign(_.clone(defaults), models[key]);
-      }
-      schema[key].name = key;
-      schema[key].options = Object.assign(_.clone(options), schema[key].options);
-    });
+      // global schema for module
+      let schema = {};
 
-    // normalize configuration
-    schema = _.reduce(schema, (result, config, name) => {
+      // populate initial schema
+      Object.keys(models).forEach((key) => {
 
-      // normalize contract key definitions into object
-      _.each(config.contract, (value, key) => {
-        if (!_.isObject(value)) {
-          config.contract[key] = { default: value };
-        }
-      });
-
-      // sanitize contract inputs (accounting for different naming)
-      _.each(config.contract, (spec, param) => {
-        if (_.has(spec, 'collapse') && _.isBoolean(spec.collapse)) {
-          spec.collapse = 'id';
-        }
-        if (!_.has(spec, 'validate') && _.has(spec, 'validation')) {
-          spec.validate = spec.validation;
-        }
-        if (!_.has(spec, 'mutate') && _.has(spec, 'mutation')) {
-          spec.mutate = spec.mutation;
-        }
-      });
-
-      // normalize queries/actions into single data structure
-      _.each(config.queries || {}, (value, key) => {
-        if (_.has(config.actions, key)) {
-          if (!_.isObject(config.actions[key])) {
-            config.actions[key] = {
-              create: config.actions[key],
-              update: config.actions[key],
-              delete: config.actions[key],
-            };
+        // model-based definition
+        if (!_.isPlainObject(models[key])) {
+          models[key].__store__ = store;
+          models[key].__name__ = key;
+          models[key].__prefix__ = prefix;
+          schema[key] = {
+            singleton: models[key].prototype instanceof Singleton,
+            api: models[key].api(),
+            contract: models[key].props(),
+            relations: models[key].relations(),
+            actions: models[key].actions(),
+            queries: models[key].queries(),
+            options: models[key].options(),
           }
+        }
+
+        // store-based definition
+        else {
+          schema[key] = Object.assign(_.clone(defaults), models[key]);
+        }
+        schema[key].prefix = prefix;
+        schema[key].name = key;
+        schema[key].options = Object.assign(_.clone(options), schema[key].options);
+      });
+
+      // normalize configuration
+      schema = _.reduce(schema, (result, config, name) => {
+
+        // normalize contract key definitions into object
+        _.each(config.contract, (value, key) => {
           if (!_.isObject(value)) {
-            value = { get: value, fetch: value };
+            config.contract[key] = { default: value };
           }
-          config.actions[key] = Object.assign(config.actions[key], value);
-        } else {
+        });
+
+        // sanitize contract inputs (accounting for different naming)
+        _.each(config.contract, (spec, param) => {
+          if (_.has(spec, 'collapse') && _.isBoolean(spec.collapse)) {
+            spec.collapse = 'id';
+          }
+          if (!_.has(spec, 'validate') && _.has(spec, 'validation')) {
+            spec.validate = spec.validation;
+          }
+          if (!_.has(spec, 'mutate') && _.has(spec, 'mutation')) {
+            spec.mutate = spec.mutation;
+          }
+        });
+
+        // normalize queries/actions into single data structure
+        _.each(config.queries || {}, (value, key) => {
+          if (_.has(config.actions, key)) {
+            if (!_.isObject(config.actions[key])) {
+              config.actions[key] = {
+                create: config.actions[key],
+                update: config.actions[key],
+                delete: config.actions[key],
+              };
+            }
+            if (!_.isObject(value)) {
+              value = { get: value, fetch: value };
+            }
+            config.actions[key] = Object.assign(config.actions[key], value);
+          } else {
+            if (!_.isObject(value)) {
+              value = { fetch: value };
+            }
+            config.actions[key] = value;
+          }
+        });
+        delete config.queries;
+
+        // normalize relation inputs
+        _.each(config.relations, (value, key) => {
           if (!_.isObject(value)) {
-            value = { fetch: value };
+            value = { model: key, url: value };
           }
-          config.actions[key] = value;
-        }
+          if (!_.isString(value.model)) {
+            value.model = value.model.__name__
+          }
+          if (_.isUndefined(value.model)) {
+            throw `Invalid model configuration for \`${key}\` relation on \`${name}\` model.`;
+          }
+          config.relations[key] = value;
+        });
+
+        result[name] = config;
+        return result;
+      }, {});
+
+      // initialize store constructs
+      const state = {};
+      const getters = {};
+      const mutations = {};
+      let actions = {};
+
+      // create store modules for each model
+      _.map(schema, (config, model) => {
+
+        // create contextualized helpers
+        const get = getterFactory(config);
+        const mutate = mutationFactory(config);
+        const act = dispatchFactory(config);
+
+        // state
+        state[model] = config.singleton ? config.default || null : {};
+
+        // getters
+        getters[`${model}`] = state => input => get.base(state, config, input);
+        getters[`${model}.one`] = state => input => get.base(state, config, input);
+        getters[`${model}.all`] = state => input => get.base(state, config, input);
+        getters[`${model}.sample`] = state => n => get.sample(state, config, n);
+        getters[`${model}.template`] = state => () => get.template(config);
+        getters[`${model}.defaults`] = state => () => get.defaults(config);
+
+        // mutations
+        mutations[`${model}.clear`] = (state, data) => mutate.clear(state, config, data);
+        mutations[`${model}.sync`] = (state, data) => mutate.sync(state, config, data);
+        mutations[`${model}.reset`] = (state, id) => mutate.reset(state, config, id);
+        mutations[`${model}.remove`] = (state, id) => mutate.remove(state, config, id);
+
+        // actions
+        actions[`${model}.fetch`] = context => act.fetch(context, config);
+        actions[`${model}.create`] = (context, data) => act.create(context, config, data);
+        actions[`${model}.update`] = (context, data) => act.update(context, config, data);
+        actions[`${model}.get`] = (context, id) => act.get(context, config, id);
+        actions[`${model}.delete`] = (context, id) => act.delete(context, config, id);
+
+        // relations
+        actions = _.assign(actions, relationFactory(schema, model), actionFactory(config));
       });
-      delete config.queries;
-
-      // normalize relation inputs
-      _.each(config.relations, (value, key) => {
-        if (!_.isObject(value)) {
-          value = { model: key, url: value };
-        }
-        if (!_.isString(value.model)) {
-          value.model = value.model.__name__
-        }
-        if (_.isUndefined(value.model)) {
-          throw `Invalid model configuration for \`${key}\` relation on \`${name}\` model.`;
-        }
-        config.relations[key] = value;
-      });
-
-      result[name] = config;
-      return result;
-    }, {});
-
-    // create store modules for each model
-    _.map(schema, (config, model) => {
-
-      // store constructs
-      const get = getterFactory(config);
-      const mutate = mutationFactory(config);
-      const act = dispatchFactory(config);
-
-      // additional actions
-      const relations = relationFactory(schema, model);
-      const actions = actionFactory(config);
 
       // register constructs
-      store.registerModule(model, {
-        namespaced: false,
-        state: {
-          [`${model}`]: config.singleton ? config.default || null : {},
-        },
-        getters: {
-          [`${model}`]: state => input => get.base(state, config, input),
-          [`${model}.one`]: state => input => get.base(state, config, input),
-          [`${model}.all`]: state => input => get.base(state, config, input),
-          [`${model}.sample`]: state => n => get.sample(state, config, n),
-          [`${model}.template`]: state => () => get.template(config),
-          [`${model}.defaults`]: state => () => get.defaults(config),
-        },
-        mutations: {
-          [`${model}.clear`]: (state, data) => mutate.clear(state, config, data),
-          [`${model}.sync`]: (state, data) => mutate.sync(state, config, data),
-          [`${model}.reset`]: (state, id) => mutate.reset(state, config, id),
-          [`${model}.remove`]: (state, id) => mutate.remove(state, config, id),
-        },
-        actions: {
-          [`${model}.fetch`]: context => act.fetch(context, config),
-          [`${model}.create`]: (context, data) => act.create(context, config, data),
-          [`${model}.update`]: (context, data) => act.update(context, config, data),
-          [`${model}.get`]: (context, id) => act.get(context, config, id),
-          [`${model}.delete`]: (context, id) => act.delete(context, config, id),
-          ...relations,
-          ...actions,
-        },
+      store.registerModule(namespace === '_' ? 'models' : namespace, {
+        namespaced: namespace !== '_',
+        state,
+        getters,
+        mutations,
+        actions,
       });
+
     });
 
     return store;
